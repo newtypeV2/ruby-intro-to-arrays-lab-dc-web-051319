7300a89d5942223bd5d8727c541b0253085adb20
Gian Libunao <gianlibunao@gmail.com>
2019-04-26 00:47:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,20 @@ end
 def array_with_two_elements
   [1,2]
 end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods
+  
+end
+
